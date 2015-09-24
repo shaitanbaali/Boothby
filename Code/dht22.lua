0be001c3b6844cf59a7a340d7a4fc09a689c6eb4
shaitanbaali <shaitan.baali@gmail.com>
2015-09-24 18:03:50 -0300

    DHT lib

--- Code/dht22.lua
+++ Code/dht22.lua
@@ -1,7 +1,7 @@
 -- ***************************************************************************
--- DHT22/11 module for ESP8266 with nodeMCU
+-- DHT22 module for ESP8266 with nodeMCU
 --
--- Written by Javier Yanez
+-- Written by Javier Yanez 
 -- but based on a script of Pigs Fly from ESP8266.com forum
 --
 -- MIT license, http://opensource.org/licenses/MIT
@@ -13,44 +13,45 @@ _G[moduleName] = M
 
 local humidity
 local temperature
-local checksum
-local checksumTest
 
 function M.read(pin)
+  local checksum
+  local checksumTest
   humidity = 0
   temperature = 0
   checksum = 0
 
   -- Use Markus Gritsch trick to speed up read/write on GPIO
-  gpio_read = gpio.read
-  gpio_write = gpio.write
-
-  bitStream = {}
+  local gpio_read = gpio.read
+  
+  local bitStream = {}
   for j = 1, 40, 1 do
     bitStream[j] = 0
   end
-  bitlength = 0
+  local bitlength = 0
 
-  -- Step 1:  send out start signal to DHT
+  -- Step 1:  send out start signal to DHT22
   gpio.mode(pin, gpio.OUTPUT)
   gpio.write(pin, gpio.HIGH)
-  tmr.delay(2000)
+  tmr.delay(150)
   gpio.write(pin, gpio.LOW)
-  tmr.delay(20000)
-  gpio.write(pin, gpio.HIGH)
+  --tmr.delay(20000)
+  tmr.delay(40)
+  --delayMicroseconds(40)
+  --gpio.write(pin, gpio.HIGH)
   gpio.mode(pin, gpio.INPUT)
 
-  -- Step 2:  DHT send response signal
+  -- Step 2:  DHT22 send response signal 
+  -- bus will always let up eventually, don't bother with timeout
+  while (gpio_read(pin) == 0 ) do end
+  local c=0
+  while (gpio_read(pin) == 1 and c < 500) do c = c + 1 end
   -- bus will always let up eventually, don't bother with timeout
   while (gpio_read(pin) == 0 ) do end
   c=0
-  while (gpio_read(pin) == 1 and c < 100) do c = c + 1 end
-  -- bus will always let up eventually, don't bother with timeout
-  while (gpio_read(pin) == 0 ) do end
-  c=0
-  while (gpio_read(pin) == 1 and c < 100) do c = c + 1 end
-
-  -- Step 3: DHT send data
+  while (gpio_read(pin) == 1 and c < 500) do c = c + 1 end
+  
+  -- Step 3: DHT22 send data
   for j = 1, 40, 1 do
     while (gpio_read(pin) == 1 and bitlength < 10 ) do
       bitlength = bitlength + 1
@@ -63,27 +64,36 @@ function M.read(pin)
 
   --DHT data acquired, process.
   for i = 1, 16, 1 do
-    if (bitStream[i + 0] > 2) then
+    if (bitStream[i] > 4) then
       humidity = humidity + 2 ^ (16 - i)
     end
-    if (bitStream[i + 16] > 2) then
+  end
+  for i = 1, 16, 1 do
+    if (bitStream[i + 16] > 4) then
       temperature = temperature + 2 ^ (16 - i)
     end
   end
   for i = 1, 8, 1 do
-    if (bitStream[i + 32] > 2) then
+    if (bitStream[i + 32] > 4) then
       checksum = checksum + 2 ^ (8 - i)
     end
   end
-  checksumTest=((humidity-humidity%256)/256+humidity%256+(temperature-temperature%256)/256+temperature%256)%256
+
+  checksumTest = (bit.band(humidity, 0xFF) + bit.rshift(humidity, 8) + bit.band(temperature, 0xFF) + bit.rshift(temperature, 8))
+  checksumTest = bit.band(checksumTest, 0xFF)
+
   if temperature > 0x8000 then
     -- convert to negative format
     temperature = -(temperature - 0x8000)
   end
 
-  if tostring(checksum) ~= tostring(checksumTest) then
-  humidity = -1
+  -- conditions compatible con float point and integer
+  if (checksumTest - checksum >= 1) or (checksum - checksumTest >= 1) then
+    humidity = nil
   end
+  --Per DHT Arduino Lib
+  gpio.mode(pin, gpio.OUTPUT)
+  gpio.write(pin, gpio.HIGH)
 end
 
 function M.getTemperature()
